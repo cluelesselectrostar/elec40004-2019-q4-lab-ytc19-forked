1da86306f045859f557a96706de7f7bc7a588f5e
hubertsumarno <hubertjosumarno@gmail.com>
2020-02-20 16:36:15 +0000

    A1_hubert

--- lab1/exam_q1/lower_case.cpp
+++ lab1/exam_q1/lower_case.cpp
@@ -1,6 +1,12 @@
 #include "lower_case.hpp"
+#include <cctype>
 
 string lower_case(const string &x)
 {
-    // IMPLEMENT ME
+	string y;
+	for(int i=0; i<x.size(); i++)
+		{
+			y.push_back(tolower(x[i]);
+		} 
+	return y;
 }
