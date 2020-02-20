946e9fc590b49efc3cc00e1bf554634695a9c703
ytc19 <ytc19@ic.ac.uk>
2020-02-20 16:39:25 +0000

    A1_Yanni

--- lab1/exam_q1/lower_case.cpp
+++ lab1/exam_q1/lower_case.cpp
@@ -1,6 +1,17 @@
 #include "lower_case.hpp"
+#include <cstring>
+#include <cstdio>
+#include <cctype>
 
 string lower_case(const string &x)
 {
-    // IMPLEMENT ME
+
+  //create test infrastructure later.
+  
+    string initialise = " ";
+    string &y = initialise;
+    for (int i=0; i<x.size(); i++)
+        y.push_back(tolower(x[i]));
+
+    return y;
 }
