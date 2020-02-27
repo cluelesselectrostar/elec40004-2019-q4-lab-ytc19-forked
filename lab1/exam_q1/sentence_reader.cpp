83f01bbfd6231755a4d982b557a1423e5fb6de8d
cluelesselectrostar <ytc19@ic.ac.uk>
2020-02-27 08:27:52 -0800

    B1_sentence_reader_recompleted

--- lab1/exam_q1/sentence_reader.cpp
+++ lab1/exam_q1/sentence_reader.cpp
@@ -1,4 +1,7 @@
 #include "sentence_reader.hpp"
+#include <cassert>
+#include <algorithm>
+#include "lower_case.hpp"
 
 SentenceReader::SentenceReader(istream &_src)
     : src(_src)
@@ -8,5 +11,50 @@ SentenceReader::SentenceReader(istream &_src)
 
 vector<string> SentenceReader::next()
 {
-    // TODO : Implement me
+  /*! Read the next sentence from the associated stream.
+
+      The vector should contain only the words, and not
+      include the terminating . or ?
+
+      An empty vector indicates that there are no more sentences and the
+      stream is finished. Once an empty sentence is returned, the method
+      should not be called again.
+  */
+
+  vector<string> sentence;
+  string word;
+
+  while (1) {
+    char tmp = src.get();
+
+    if (src.fail()){
+      sentence.clear(); //defensive programming: want to return empty sentence.
+      //assert(sentence.empty());
+      return sentence;
+    } else {
+
+        if (tmp == '?' | tmp == '.') {
+          if (!word.empty()) { //this is the last word of the sentence
+            sentence.push_back(word);
+            word.clear(); //defensive programming.
+          }
+          assert (!sentence.empty());
+          return sentence; //this is where the function should end; returning a vector of strings (a sentence)
+        }
+
+        if (isalpha(tmp)) {
+          word.push_back(tolower(tmp));
+        } else { //if the character is empty (space), its the end of the word.
+            if (!word.empty()) {
+              sentence.push_back(word);
+              word.clear();
+            }
+        }
+
+    }
+
+
+
+  }
+
 }
