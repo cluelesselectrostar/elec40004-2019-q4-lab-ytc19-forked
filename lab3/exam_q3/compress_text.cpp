44030ae15053e483545a645451edbe9042b54b9b
hubertsumarno <hubertjosumarno@gmail.com>
2020-03-31 08:15:33 +0100

    finished_compresse_text.cpp

---
+++ lab3/exam_q3/compress_text.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "bimap.hpp"
+
+int main()
+{
+	BiMap* prints = BiMap_create();
+	int count = 0;	
+	int dummy = 0;
+
+	cout << std::hex;
+
+	while(1){
+		string tmp;
+		
+		cin >> tmp;
+
+		if(cin.fail()){
+			break;
+		}
+		if(BiMap_lookup(prints, tmp, dummy)){
+			cout << "@" << dummy << " ";
+		}else{
+			dummy = count;
+			BiMap_insert(prints, tmp, count);
+			count++;
+			cout << tmp << " ";
+		}
+	}
+	BiMap_destroy(prints);
+}
