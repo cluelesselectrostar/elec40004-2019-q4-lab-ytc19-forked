1f0d2bfe20fdde2882dd2fab3cfe7b7a687013bf
hubertsumarno <hubertjosumarno@gmail.com>
2020-04-09 13:30:58 +0100

    fixed_B1

--- lab4/exam_q4/simple_power_estimator.cpp
+++ lab4/exam_q4/simple_power_estimator.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-vector<bool> read_bits(string s)
+vector<bool> read_bit(string s)
 {
 	vector<bool> out;
 	for(int i=0; i<s.length(); i++){
@@ -22,36 +22,41 @@ vector<bool> read_bits(string s)
 
 int main(int argc, char** argv)
 {
-	double per_energy_signal = 1e-12;
-	double clock_rate = 100e6;
+	double per_energy_signal;
+	double clock_rate;
 	if(argc>=3){
 		clock_rate = stod(argv[1]);
 		per_energy_signal = stod(argv[2]);
+	}else{
+		clock_rate = 100e6;
+		per_energy_signal = 1e-12;
 	}
 	
-	int no_of_signals;
+	int signals;
 	int cycle_count = -1;
-	int total_flips = 1;
+	int total_flips = 0;
 	double power;
-	vector<bool> current_cycle;
-	vector<bool> previous_cycle;
 	string cc;
+	string pc;
 
 	while(!cin.fail()){
 		cin >> cc;
-		current_cycle = read_bits(cc);
-		if(cycle_count>=1){
-			vector<int> flip_positions = bit_flip_positions_in_vector(current_cycle, previous_cycle);
-			total_flips = total_flips + flip_positions.size();
+		if(cycle_count>=0){
+			if(cc!=pc){
+				vector<bool> vcc = read_bit(cc);
+				vector<bool> vpc = read_bit(pc);
+				vector<int> flip_positions = bit_flip_positions_in_vector(vcc, vpc);
+				total_flips += flip_positions.size();
+			}
 		}
-		previous_cycle = current_cycle;
+		pc = cc;
 		cycle_count++;
 	}
 
-	no_of_signals = current_cycle.size();
-	power = (total_flips*per_energy_signal)/(cycle_count*clock_rate);
+	signals = cc.size();
+	power = (total_flips*per_energy_signal)/(cycle_count/clock_rate);
 
-	cout << no_of_signals << endl;
+	cout << signals << endl;
 	cout << cycle_count << endl;
 	cout << total_flips << endl;
 	cout << power << endl;
