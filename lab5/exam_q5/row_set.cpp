82d39febcb027a3524d12c1c3eaef498923bd5e5
cluelesselectrostar <ytc19@ic.ac.uk>
2020-04-14 00:29:00 -0700

    A1, A3, B1 Completed

--- lab5/exam_q5/row_set.cpp
+++ lab5/exam_q5/row_set.cpp
@@ -2,9 +2,12 @@
 
 #include "row_echelon_form.hpp"
 
+// B = B - A[r,c] = -(-B + A(r,c))
 void RowSet::SubBuffer(int r, int c)
 {
-    // TODO
+  NegBuffer();
+  AddBuffer(r,c);
+  NegBuffer();
 }
 
 void RowSet::DivBuffer(int r, int c)
@@ -22,9 +25,19 @@ void RowSet::ShowSparsity(ostream &dst)
     // TODO
 }
 
+// Prints the given row to `dst`. The row should all be on one
+// line, with one space between entries. No end-of-line character
+// should be added.
 void RowSet::WriteRow(int row, ostream &dst)
 {
-    // TODO
+    for (int i=0; i<ColCount(); i++) {
+      LoadBuffer(row, i);
+      if (i== ColCount()-1) {
+        WriteBuffer(dst);
+      }
+      WriteBuffer(dst);
+      dst << " ";
+    }
 }
 
 void RowSet::WriteMatrix(ostream &dst)
--- lab5/exam_q5/test_field.cpp
+++ lab5/exam_q5/test_field.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     }
 
     RowSet *A = create_row_set(field);
-    
+
     // We need at least one element to perform the tests,
     // in addition to the buffer
     A->Resize(1,1);
@@ -20,13 +20,34 @@ int main(int argc, char *argv[])
     /////////////////////////////////////////
     // Insert tests here
 
-    // TODO - Additive identity: `add(0,1) == `1` : 
+    // TODO - Additive identity: `add(0,1) == `1` :
+    A->ZeroBuffer();
+    A->StoreBuffer(1,1);
+    A->UnitBuffer();
+    A->AddBuffer(1,1);
+    A->StoreBuffer(1,1);
+    assert(A->IsUnit(1,1));
 
     // TODO - Multiplicative identity: `mul(1,1) == `1`
+    A->UnitBuffer();
+    A->StoreBuffer(1,1);
+    A->UnitBuffer();
+    A->MulBuffer(1,1);
+    A->StoreBuffer(1,1);
+    assert(A->IsUnit(1,1));
 
     // TODO - Double negate is an identity: `neg(neg(1)) == 1`
+    A->UnitBuffer();
+    A->NegBuffer();
+    A->NegBuffer();
+    A->StoreBuffer(1,1);
+    assert(A->IsUnit(1,1));
 
     // TODO - Unit is it's own inverse: ` inv(1) == 1`
+    A->UnitBuffer();
+    A->InvBuffer();
+    A->StoreBuffer(1,1);
+    assert(A->IsUnit(1,1));
 
 
 }
